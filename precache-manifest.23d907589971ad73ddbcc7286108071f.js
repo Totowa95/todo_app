4ed45e3b29afcf7a45175198e9129080c93e7a01
Anton <a.v.sorokin95@gmail.com>
2020-09-22 18:45:14 +0300

    Updates

---
+++ precache-manifest.23d907589971ad73ddbcc7286108071f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a303f26570b54d5c9206543c9e4f7d5",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "d8dd7c749dde218c038b",
+    "url": "/todo_app/static/css/main.0a988138.chunk.css"
+  },
+  {
+    "revision": "917e5e56a8d5a4ea25f7",
+    "url": "/todo_app/static/js/2.7fa0d39c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo_app/static/js/2.7fa0d39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8dd7c749dde218c038b",
+    "url": "/todo_app/static/js/main.3dc90c1c.chunk.js"
+  },
+  {
+    "revision": "ced6bf582c0630731099",
+    "url": "/todo_app/static/js/runtime-main.45d19e70.js"
+  }
+]);
