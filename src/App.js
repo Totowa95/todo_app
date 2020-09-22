ee0639b84245ac800e52befb24bfb43bc45ca90b
Anton <a.v.sorokin95@gmail.com>
2020-09-22 18:40:45 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, useEffect } from 'react';
+import { TodoApp } from './components/TodoApp';
+import { TodoList } from './components/TodoList';
+import { TodosFilter } from './components/TodosFilter';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [filterBy, setFilterBy] = useState('all');
+
+  useEffect(() => {
+    if (localStorage.getItem('list') !== null) {
+      setTodos(JSON.parse(localStorage.getItem('list')));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(todos));
+  }, [todos]);
+
+  const filteredTodos = useMemo(() => todos.filter(todo => ((filterBy === 'all')
+    ? todo
+    : todo.completed === filterBy)), [filterBy, todos]);
+
+  const getTodo = (id, title, completed = false) => {
+    setTodos([...todos, {
+      id,
+      title,
+      completed,
+    }]);
+  };
+
+  const editingTodo = (id, newTitle) => {
+    setTodos(todos.map(todo => (todo.id === id
+      ? ({ ...todo, title: newTitle })
+      : ({ ...todo }))));
+  };
+
+  const changeCompleted = (id) => {
+    setTodos(todos.map(todo => ((todo.id === id)
+      ? { ...todo, completed: !todo.completed }
+      : todo)));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const clearCompleted = () => {
+    setTodos(todos.filter(todo => todo.completed === false));
+  };
+
+  const toggleCompleted = () => {
+    if (todos.length && todos.every(todo => todo.completed)) {
+      setTodos(todos.map(todo => ({ ...todo, completed: false })));
+
+      return;
+    }
+
+    setTodos(todos.map(todo => ({ ...todo, completed: true })));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos App</h1>
+
+        <TodoApp getTodo={getTodo} />
       </header>
-    </div>
+
+      <section className="main">
+        <input
+          type="checkbox"
+          id="toggle-all"
+          className="toggle-all"
+          checked={todos.length > 0 && todos.every(todo => todo.completed)}
+          onClick={() => toggleCompleted()}
+          readOnly
+        />
+        <label
+          htmlFor="toggle-all"
+        >
+          Mark all as complete
+        </label>
+
+        <TodoList
+          todos={filteredTodos}
+          changeCompleted={changeCompleted}
+          deleteTodo={deleteTodo}
+          editingTodo={editingTodo}
+        />
+      </section>
+
+      {
+        !todos.length
+        || (
+          <TodosFilter
+            todosLeft={
+              todos
+                .filter(todo => todo.completed === false)
+                .length
+            }
+            showTodos={setFilterBy}
+            clearCompleted={clearCompleted}
+            completedTodos={todos.some(todo => todo.completed)}
+          />
+        )
+      }
+    </section>
   );
 }
 
